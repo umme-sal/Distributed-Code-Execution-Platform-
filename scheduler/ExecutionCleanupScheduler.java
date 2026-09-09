66f6d976835d165a49fabc802228f4ce5b211a38
Umme Salma <ummes1156@gmail.com>
2026-09-09 11:57:39 +0530

    Add files via upload

--- scheduler/ExecutionCleanupScheduler.java
+++ scheduler/ExecutionCleanupScheduler.java
@@ -1,1 +1,25 @@
+package com.example.project.codeexecutor.scheduler;
 
+import com.example.project.codeexecutor.repository.ExecutionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class ExecutionCleanupScheduler
+{
+    private final ExecutionRepository executionRepository;
+
+    @Scheduled(cron = "0 0 2 * * ?")
+    public void cleanupExpiredExecutions()
+    {
+        executionRepository.deleteByExpiresAtBefore(
+                LocalDateTime.now()
+        );
+
+        System.out.println("Expired executions deleted.");
+    }
+}
